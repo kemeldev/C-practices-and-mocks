4f7996d2dd23922a57f427ccbfa043724e1d67de
kemeldev <kemel.developer@gmail.com>
2024-01-15 15:14:20 -0600

    Add project files.

---
+++ segundaAplicacion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace segundaApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxInt = int.MaxValue;
+
+            // This will throw System.OverflowException
+            checked
+            {
+                int overflowResult = maxInt + 1;
+                Console.WriteLine("Overflow result with checked: " + overflowResult);
+            }
+        }
+    }
+}
